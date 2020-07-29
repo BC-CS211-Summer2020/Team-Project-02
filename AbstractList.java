d8ce627f4ed9f37f17665dff054a80a19497a0f9
angelazou-bellevuecollege <68084038+angelazou-bellevuecollege@users.noreply.github.com>
2020-07-28 21:10:56 -0700

    Add files via upload

---
+++ AbstractList.java
@@ -0,0 +1,19 @@
+package teamProject02;
+
+public abstract class AbstractList<E> {
+
+	/*
+	 * methods:
+	 *     public int size();
+    public E get(int index);
+    public int indexOf(E value);
+    public boolean isEmpty();
+    public boolean contains(E value);
+    public void add(E value);
+    public void add(int index, E value);
+    public void remove(int index);
+    public void set(int index, E value);
+    public void clear();
+	 */
+	
+}
