5487732c295c6c71a89f1c7142f8284fd9dcf95b
angelazou-bellevuecollege <68084038+angelazou-bellevuecollege@users.noreply.github.com>
2020-07-28 21:41:28 -0700

    Update AbstractList.java

--- AbstractList.java
+++ AbstractList.java
@@ -18,9 +18,27 @@ public abstract class AbstractList<E> {
 	 */
 	private int size;
 	
-	public int size() {
+	public int size() { //returns size
+	return size;	
+	}
+	
+	public E get(int index) { //returns value at given index
 		
 	}
 	
-	public E get(int index)
+	public int indexOf(E value) { //returns the index of the first appearance of the given value
+		
+	}
+	
+	public boolean isEmpty() { //returns true if empty and false if not empty
+		
+	}
+	
+	public boolean contains(E value) { //returns true if the list contains the given value and false if it does not
+		
+	}
+	
+	public void add(E value) { // adds given value to the end of the list
+		
+	}
 }
