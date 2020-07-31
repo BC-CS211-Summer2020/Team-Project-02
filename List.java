5df17f1950e3163c87fa02cdb137651ddae80a5c
Filbert-code <42334044+Filbert-code@users.noreply.github.com>
2020-07-31 15:27:13 -0700

    Added addAll() method

--- List.java
+++ List.java
@@ -8,6 +8,7 @@ public interface List<E> extends Iterable<E> {
     public boolean contains(E value);
     public void add(E value);
     public void add(int index, E value);
+    public void addAll(List<E> other);
     public void remove(int index);
     public void set(int index, E value);
     public void clear();
