a7e2e6f19331714466163d1769891f75fce8700a
melinaperraut <67724278+melinaperraut@users.noreply.github.com>
2020-08-01 19:58:52 -0700

    Update List.java
    Added method header

--- List.java
+++ List.java
@@ -1,3 +1,19 @@
+/*CSS 211 Summer 2020
+ * Team #2
+ * Alex Filbert
+ * Diane Lansinger (project lead)
+ * Melina Perraut (code co-lead)
+ * Zhexiu Tan
+ * Arash Yazdidoost
+ * Angie Zou (code co-lead)
+ * 
+ * Team Project #2
+ * Building Java Programs (Reges & Stepp, 5th edition)
+ * Ch. 16, p. 1027, Programming Exercise #3
+ * TeamProject02
+ *
+ * Final Submission:  08-03-20
+*/
 // Generic interface for a List of E objects.
 
 public interface List<E> extends Iterable<E> {
